c43a746ddac3e03b78ac991b6bcb01e287c3a8ad
Алексей <102001074+marumm@users.noreply.github.com>
2024-03-30 21:50:31 +0300

    Update query.sql

--- Simulator DS/SQL Query/query.sql
+++ Simulator DS/SQL Query/query.sql
@@ -1,5 +1,4 @@
-SELECT date_trunc('week', date) as week, SUM(amount) as sum_receipt
+SELECT date_trunc('week', date) as weeks, SUM(amount) as sum_receipt
 FROM new_payments
-WHERE status='Confirmed'
-GROUP BY week
-ORDER BY week
+GROUP BY weeks
+ORDER BY weeks
