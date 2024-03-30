ad1f7df09cf5129893f57e4415fac8ddde0f9c39
Алексей <102001074+marumm@users.noreply.github.com>
2024-03-30 22:37:00 +0300

    Create ARPU & AOV.sql

---
+++ Simulator DS/SQL Query/ARPU & AOV.sql
@@ -0,0 +1,5 @@
+SELECT date(date_trunc('month', date)) as time, SUM(amount)/COUNT(DISTINCT(email_id)) as arppu, SUM(amount)/COUNT(amount) as aov
+FROM new_payments
+WHERE status='Confirmed'
+GROUP BY time
+ORDER BY time
