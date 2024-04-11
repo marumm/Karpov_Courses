5530bef7883f8a40bd71b89c0307ad84cb6a6338
Алексей <102001074+marumm@users.noreply.github.com>
2024-04-11 15:31:29 +0300

    Update ARPU & AOV.sql

--- Simulator DS/SQL Query/ARPU & AOV.sql
+++ Simulator DS/SQL Query/ARPU & AOV.sql
@@ -1,3 +1,4 @@
+--Запрос для вывода arppu (средняя прибыль с пользователя) и aov (средний чек)
 SELECT date(date_trunc('month', date)) as time, SUM(amount)/COUNT(DISTINCT(email_id)) as arppu, SUM(amount)/COUNT(amount) as aov
 FROM new_payments
 WHERE status='Confirmed'
